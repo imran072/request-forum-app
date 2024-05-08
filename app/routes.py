6b62d118cf858790046f98d28c278c7815bce620
Phoebus <phoebus@Phoebuss-MacBook-Air.local>
2024-05-08 18:59:00 +0800

    when car is selected from index.html, it is the car filtered in search.html as well

--- app/routes.py
+++ app/routes.py
@@ -1,32 +1,50 @@
-from flask import Blueprint, render_template, url_for, request
+from flask import Blueprint, render_template, request
 from .models import Vehicle
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    makes = ['Audi', 'Bentley', 'BMW', 'BYD', 'Chery', 'Chevrolet', 'Fiat', 'Fisker', 'Ford', 'Honda', 'Hyundai', 'Jaguar', 'Kia', 'Lexus', 'Maserati', 'Mahindra', 'Mercedes-Benz', 'MG', 'Mitsubishi', 'Nissan', 'Peugeot', 'Porsche', 'Opel', 'Renault', 'Suzuki', 'Tesla', 'Toyota', 'Volvo', 'Volkswagen']
-    distances = [5, 10, 15, 20, 30, 40, 50, 75, 100, 150, 200]
-    prices = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000, 16000, 17000, 18000, 19000, 20000, 25000, 30000, 35000, 40000, 45000, 50000]
-    ads = [
-        {'name': 'Tesla Model 3', 'price': 25000, 'description': 'Model 3 is designed for electric-powered performance, with quick acceleration, long range and fast charging.', 'image': 'Tesla_Model_3.png', 'alt': 'Tesla_Model_3'},
-        {'name': 'Toyota Camry', 'price': 20000, 'description': 'Toyota Electrified - Towards the future', 'image': 'Toyota_Camry.png', 'alt': 'Toyota_Camry'},
-        {'name': 'Tesla Model S', 'price': 28000, 'description': 'Model 3 is designed for electric-powered performance, with quick acceleration, long range and fast charging.', 'image': 'Tesla_Model_X.png', 'alt': 'Tesla_Model_S'}
-    ]
-    brand_logos = ['Tesla.png', 'Honda.png', 'Fisker.png', 'Ford.png', 'Audi.png', 'Renault.png', 'Toyota.png', 'Volvo.png', 'Maserati.png', 'Nissan.png', 'Mahindra.png', 'Kia.png', 'Jaguar.png', 'Hyundai.png', 'BYD.png', 'Volkswagen.png', 'Bentley.png', 'BMW.png']
+    makes = ['Tesla', 'BMW', 'Nissan']
+    prices = [20000, 40000, 60000]
+    distances = [100, 200, 300]
 
-    print(url_for('main.index'))
-    return render_template('index.html', makes=makes, distances=distances, prices=prices, ads=ads, brand_logos=brand_logos)
+    ads = [
+        {
+            'name': 'Tesla Model S 100D Long Range',
+            'price': 79995.0,
+            'description': 'All Glass Panoramic Roof, Premium Interior and Lighting.',
+            'image': 'tesla_model_s_100d_long_range.jpg',
+            'alt': 'Tesla Model S 100D Long Range'
+        },
+        {
+            'name': 'Tesla Model S E 85D',
+            'price': 47995.0,
+            'description': 'All Glass Panoramic Roof, Alloy Wheels.',
+            'image': 'tesla_model_s_e_85d.jpg',
+            'alt': 'Tesla Model S E 85D'
+        }
+    ]
+
+    brand_logos = [
+        'tesla_logo.png',
+        'bmw_logo.png'
+    ]
+
+    return render_template('index.html', makes=makes, prices=prices, distances=distances, ads=ads, brand_logos=brand_logos)
 
 @main.route('/vehicles', methods=['GET'])
 def vehicles():
     make = request.args.get('make', 'any')
     model = request.args.get('model', 'any')
     year = request.args.get('year', 'any')
+    from_year = request.args.get('from_year', 'any')
+    to_year = request.args.get('to_year', 'any')
     mileage = request.args.get('mileage', 'any')
     top_speed = request.args.get('top_speed', 'any')
     acceleration = request.args.get('acceleration', 'any')
-    price = request.args.get('price', 'any')
+    min_price = request.args.get('min_price', 'any')
+    max_price = request.args.get('max_price', 'any')
     color = request.args.get('color', 'any')
 
     query = Vehicle.query
@@ -35,16 +53,22 @@ def vehicles():
         query = query.filter_by(make=make)
     if model != 'any':
         query = query.filter_by(model=model)
+    if from_year != 'any':
+        query = query.filter(Vehicle.year >= int(from_year))
+    if to_year != 'any':
+        query = query.filter(Vehicle.year <= int(to_year))
     if year != 'any':
-        query = query.filter_by(year=int(year))
+        query = query.filter(Vehicle.year == int(year))
     if mileage != 'any':
         query = query.filter(Vehicle.mileage <= int(mileage))
     if top_speed != 'any':
         query = query.filter(Vehicle.top_speed >= int(top_speed))
     if acceleration != 'any':
         query = query.filter(Vehicle.acceleration <= float(acceleration))
-    if price != 'any':
-        query = query.filter(Vehicle.price <= float(price))
+    if min_price != 'any':
+        query = query.filter(Vehicle.price >= float(min_price))
+    if max_price != 'any':
+        query = query.filter(Vehicle.price <= float(max_price))
     if color != 'any':
         query = query.filter_by(color=color)
 
@@ -53,8 +77,8 @@ def vehicles():
     makes = [v.make for v in Vehicle.query.distinct(Vehicle.make)]
     models = [v.model for v in Vehicle.query.distinct(Vehicle.model)]
     years = sorted({v.year for v in Vehicle.query.distinct(Vehicle.year)})
-    mileage_options = [5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 50000, 60000, 70000, 80000]
-    top_speed_options = [100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300]
+    mileage_options = [10000, 20000, 30000, 40000]
+    top_speed_options = [150, 200, 250, 300]
     acceleration_options = [3.2, 5.6, 7.8]
     price_options = [30000, 40000, 50000]
     color_options = ['Red', 'Blue', 'Black']
@@ -64,10 +88,13 @@ def vehicles():
         make=make,
         model=model,
         year=year,
+        from_year=from_year,
+        to_year=to_year,
         mileage_value=mileage,
         top_speed_value=top_speed,
         acceleration_value=acceleration,
-        price_value=price,
+        min_price=min_price,
+        max_price=max_price,
         color=color,
         vehicles=vehicles,
         makes=makes,
