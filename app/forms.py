b6e3e94e24754ba72a622920553478d0ed32a060
zhanerken <zhanerken@gmail.com>
2024-05-14 11:21:01 +0800

    add listing first working draft

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField, FloatField, SelectField, validators, PasswordField, Form
+from wtforms import StringField, IntegerField, SubmitField, FloatField, SelectField, validators, PasswordField, Form, FileField
 from wtforms.validators import DataRequired, Optional, InputRequired, Length, EqualTo, ValidationError, Email
 from .models import User
-
+from flask_wtf.file import FileRequired, FileAllowed
 
 class SearchForm(FlaskForm):
     make = StringField('Make', validators=[Optional()])
@@ -31,3 +31,19 @@ class ResetPasswordForm(FlaskForm):
     password = PasswordField('New Password', validators=[DataRequired()])
     password2 = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Reset Password')
+
+
+class AddListingForm(FlaskForm):
+    make = StringField('Make', validators=[DataRequired()])
+    model = StringField('Model', validators=[DataRequired()])
+    year = IntegerField('Year', validators=[DataRequired()])
+    mileage = IntegerField('Mileage', validators=[DataRequired()])
+    battery_capacity = IntegerField('Battery Capacity (in kWh)', validators=[DataRequired()])
+    color = StringField('Color', validators=[DataRequired()])
+    price = FloatField('Price ($)', validators=[DataRequired()])
+    doors = IntegerField('Number of Doors', validators=[DataRequired()])
+    car_type = SelectField('Car Type', choices=[('Sedan', 'Sedan'), ('SUV', 'SUV'), ('Hatchback', 'Hatchback'), ('Coupe', 'Coupe')], validators=[DataRequired()])
+    top_speed = IntegerField('Top Speed (km/h)', validators=[DataRequired()])
+    acceleration = FloatField('0-100 km/h Acceleration (seconds)', validators=[DataRequired()])
+    image = FileField('Car Image', validators=[FileRequired(), FileAllowed(['jpg', 'png'], 'Images only!')])
+    submit = SubmitField('Add Listing')
